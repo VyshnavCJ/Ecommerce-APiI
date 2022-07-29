db6ac83a082b4ad7f7e2c22c00578903926b9278
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2022-07-29 06:28:08 +0530

    Hash passoword included

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log('E-Commerce API');
+require("express-async-errors");
+require("dotenv").config();
+const express = require("express");
+
+//error handlers
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//app
+const app = express();
+
+//MongoDB connection
+const connectDB = require("./db/connect");
+const morgan = require("morgan");
+
+//routes
+const authRouter = require("./routes/authRoutes");
+
+//port
+const port = process.env.PORT || 5000;
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+//middleware
+
+app.get("/", (req, res) => {
+  res.send("E-commerce=api");
+});
+
+app.use("/api/v1/auth", authRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+//start function
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server listening at port ${port}`);
+    });
+  } catch (error) {
+    console.log("Something went wrong");
+  }
+};
+
+start();
