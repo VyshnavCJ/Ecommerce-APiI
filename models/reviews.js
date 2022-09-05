3a4672acdecfa771b9dfe70c88d9f1994777ffe1
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2022-09-05 20:11:17 +0530

    product model complete

--- models/reviews.js
+++ models/reviews.js
@@ -32,5 +32,38 @@ const ReviewSchema = mongoose.Schema(
   { timestamps: true }
 );
 
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "$rating" },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+ReviewSchema.post("save", async function () {
+  this.constructor.calculateAverageRating(this.product);
+});
+
+ReviewSchema.post("remove", async function () {
+  this.constructor.calculateAverageRating(this.product);
+});
+
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 module.exports = mongoose.model("Review", ReviewSchema);
