c44cf78992491b608cb189a9e0dc90ec6c4063e3
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2022-09-05 22:31:59 +0530

    security added

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ const connectDB = require("./db/connect");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 //routes
 const authRouter = require("./routes/authRoutes");
@@ -28,6 +33,18 @@ const reviewRouter = require("./routes/reviewRouters");
 const orderRouter = require("./routes/orderRoutes");
 //port
 const port = process.env.PORT || 5000;
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan("tiny"));
 app.use(express.json());
