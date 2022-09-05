297d5fed1f7fc1697240cfa5df4eb02c30427da6
Vyshnav C J <vyshnavjenilkumar@gmail.com>
2022-09-05 23:02:48 +0530

    small fixes

--- app.js
+++ app.js
@@ -46,21 +46,12 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./public"));
 app.use(fileUpload());
 //middleware
 
-app.get("/", (req, res) => {
-  res.send("E-commerce api");
-});
-
-app.get("/api/v1", (req, res) => {
-  console.log(req.signedCookies);
-  res.send("E-commerce api");
-});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
